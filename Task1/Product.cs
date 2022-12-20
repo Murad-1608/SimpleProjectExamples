9f1c033ce510d38f60c5eba121bca8eaf324eafe
Murad <murad.yunus.2017@gmail.com>
2022-12-20 17:11:07 +0400

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price, int count)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Count = count;
+        }
+
+        public string Name;
+        public double Price;
+        private int Count;
+        private double Totalncome;
+
+        public void Sell()
+        {
+            if (this.Count == 0) Console.WriteLine("Mehsul yoxdur");
+            else
+            {
+                this.Count--;
+                this.Totalncome += Price;
+            }
+        }
+    }
+}
